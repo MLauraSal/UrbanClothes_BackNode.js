1be895addb67559019ba886b181ad9190c7cd93c
Mariana <valenarte@gmail.com>
2025-12-08 10:45:29 -0300

    On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	modified:   index.js
    	modified:   vercel.json

--- index.js
+++ index.js
@@ -16,11 +16,22 @@ app.use(cors({
   credentials: true,
 }));
 
+
+
+
 // Routes
+
+
+
 app.use('/api/products', productsRoutes);
 app.use('/api/users', userRoutes); 
 app.use('/api/auth', authRoutes);
 app.use('/api/categories', categoriesRoutes);
+
+
+app.use((req, res, next) => {
+  res.status(404).json({ error: "Not Found" });
+});
 
 // Start the server
 const {port} = configDotenvPort ();
