797ca921e11413538c87d8fd3976009f0eb3a6c6
Mariana <valenarte@gmail.com>
2025-12-09 14:36:04 -0300

    On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	modified:   index.js

--- index.js
+++ index.js
@@ -30,9 +30,6 @@ app.use('/api/auth', authRoutes);
 app.use('/api/categories', categoriesRoutes);
 
 
-app.use((req, res, next) => {
-  res.status(404).json({ error: "Not Found" });
-});
 
 // Start the server
 const {port} = configDotenvPort ();
